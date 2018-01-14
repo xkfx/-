0b112e404fe6fa825d6d3ce4fe597b408a3644f8
xkfx <531194294@qq.com>
2018-01-14 19:01:09 +0800

    do some simple refatoring ...

--- src/main/java/chatroom/server/controller/ServerController.java
+++ src/main/java/chatroom/server/controller/ServerController.java
@@ -21,17 +21,31 @@ import static chatroom.common.message.Iconst.*;
 
 public class ServerController {
 
-    private static final int PORT = 10000;
-    private static final String PROMPT = "The server is listening on 10000 port ...";
+    private static final int PORT = 10000; // static 的特点是慢 占用空间小，但是因为只用一次就无所谓慢了
 
     private final MessageService messageService = new MessageServiceImpl();
     private final UserService userService = new UserServiceImpl();
 
+    private ObjectInputStream getObjectInputStream(final Socket socket) throws IOException {
+        ObjectInputStream result = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+        return result;
+    }
+
+    private Message getFirstMessage(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        Object object = objectInputStream.readObject();
+        Message result = (Message) object;
+        return result;
+    }
+
     private void continuedResponse(ObjectInputStream inputStream, Socket socket) throws IOException, ClassNotFoundException {
         System.out.println("准备持续为此Socket提供服务······");
+        final int contentSize = 32; // 一般单条消息的长度是 20 个汉字以内，当 > 16 时会成倍扩张，所以干脆直接申请 32 个字节，以提高性能
+
         while (true) {
             Object object = inputStream.readObject();
             Message message = (Message) object;
+
+
 
             if (message.getType() == PUBLIC_MESSAGE) {
                 messageService.sendAll(socket, message);
@@ -44,14 +58,14 @@ public class ServerController {
                     // 在线转发
                     Socket targetSocket = userService.getSocket(target);
                     final String originContent = message.getContent(); // 不加 final 只是传引用！暂时还没有思考其它方法。
-                    StringBuilder builder = new StringBuilder();
+                    StringBuilder builder = new StringBuilder(contentSize);
                     builder.append(userService.getUser(message.getSource()).getNickname() + "悄悄对你说：");
                     builder.append(originContent + "\n");
 
                     message.setContent(builder.toString());
                     messageService.send(targetSocket, message);
 
-                    StringBuilder echo = new StringBuilder();
+                    StringBuilder echo = new StringBuilder(contentSize);
                     echo.append("你悄悄对" + userService.getUser(target).getNickname() + "说：");
                     echo.append(originContent + "\n");
 
@@ -67,62 +81,58 @@ public class ServerController {
         }
     }
 
-    private void serveSocket(final Socket socket) {
+    private void createServerThread(final Socket socket) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                printPrompt(socket.hashCode() + " accesses the server.");
-                ObjectInputStream inputStream = null;
-
+                printPrompt("Create a new thread for " + socket.hashCode());
+                ObjectInputStream objectInputStream = null;
                 try {
-                    inputStream = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-                    System.out.println("服务器输入流创建完毕，等待客户端的第一条消息······");
-                    // 响应该 socket 的第一条消息
-                    Object object = inputStream.readObject();
-                    Message firstRequest = (Message) object;
-                    Message firstResponse = null;
+                    objectInputStream = getObjectInputStream(socket);
+                    printPrompt("ObjectInputStream has been created, waiting for the first client message ...");
+                    Message message = getFirstMessage(objectInputStream);
 
                     // 游客
-                    if (firstRequest.getType() == VISITOR_ACCESS) {
-                        VisitorLogin visitorLogin = (VisitorLogin) firstRequest;
+                    if (message.getType() == VISITOR_ACCESS) {
+                        VisitorLogin visitorLogin = (VisitorLogin) message;
                         String nickname = visitorLogin.getNickname();
 
                         Visitor visitor = new Visitor(nickname);
                         messageService.addAcceptor(socket, visitor);
                         System.out.println("昵称为" + nickname + "的游客等登陆服务器······");
 
-                        firstResponse = Message.ok("ok");
+                        Message firstResponse = Message.ok("ok");
                         messageService.send(socket, firstResponse);
 
-                        continuedResponse(inputStream, socket);
+                        continuedResponse(objectInputStream, socket);
                     }
 
                     // 注册
-                    if (firstRequest.getType() == GENERAL_REGISTER) {
+                    if (message.getType() == GENERAL_REGISTER) {
 
-                        MsgRegister msgRegister = (MsgRegister) firstRequest;
+                        MsgRegister msgRegister = (MsgRegister) message;
                         String username = msgRegister.get("username");
                         String password = msgRegister.get("password");
 
                         System.out.println(username + "&" + password + "准备注册");
 
                         Register reg = new Register(username, password);
-                        firstResponse = userService.register(reg);
+                        Message firstResponse = userService.register(reg);
 
                         messageService.send(socket, firstResponse);
                     }
 
                     // 登陆
-                    if (firstRequest.getType() == GENERAL_LOGIN) {
+                    if (message.getType() == GENERAL_LOGIN) {
 
-                        MsgLogin msgLogin = (MsgLogin) firstRequest;
+                        MsgLogin msgLogin = (MsgLogin) message;
                         String username = msgLogin.get("username");
                         String password = msgLogin.get("password");
 
                         System.out.println(username + "&" + password + "准备登陆");
 
                         Login login = new Login(username, password);
-                        firstResponse = userService.login(socket, login);
+                        Message firstResponse = userService.login(socket, login);
 
                         messageService.send(socket, firstResponse);
 
@@ -138,7 +148,7 @@ public class ServerController {
                                 messageService.send(socket, new MsgFriends(userList));
                                 System.out.println("用户好友列表已经发出" + userList);
                             }
-                            continuedResponse(inputStream, socket);
+                            continuedResponse(objectInputStream, socket);
                         }
                     }
 
@@ -156,19 +166,13 @@ public class ServerController {
                     System.out.println(socket.hashCode() + "已正常下线。IOException\n");
                 } finally {
                     try {
-                        if (inputStream != null) {
-                            inputStream.close();
+                        if (objectInputStream != null) {
+                            objectInputStream.close();
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    try {
                         socket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             }
         }).start();
@@ -185,14 +189,15 @@ public class ServerController {
 
     private void startWorking(final ServerSocket serverSocket) throws IOException {
         while (true) {
-            serveSocket(serverSocket.accept());
+            Socket socket = serverSocket.accept(); // 关于引用放 while外面还是里面有激烈的争论，我的结论是放里面。
+            createServerThread(socket);
         }
     }
 
     public void startup() {
         try {
             ServerSocket serverSocket = createServerSocket(PORT);
-            printPrompt(PROMPT);
+            printPrompt("The server is listening on " + PORT + " port ...");
             startWorking(serverSocket);
         } catch (IOException e) {
             e.printStackTrace();
